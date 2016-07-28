6dd1d62598f2ad54f28ee3ee93d6f24b79b1296a
Jonathan Szablevski <jszablevski@hotmail.com>
2016-07-27 22:48:54 -0300

    Generate post image

--- generate-card.php
+++ generate-card.php
@@ -28,7 +28,8 @@ if(isset($token)){
 	}
 
 	$params = get_cardmaker_params($user);
-	$monster_data = get_monster_for($user);
+	$monster_name = get_monster_for($user);
+	$monster_data = get_monster_data($monster_name);
 
 	$type = preg_split('/\//i', $monster_data['type']);
 	$params += [
@@ -42,7 +43,10 @@ if(isset($token)){
 		'atk' => $monster_data['atk'],
 		'def' => $monster_data['def'],
 	];
-	$img_data = generate_card($params);
+	$user_img_path = generate_card($params);
 
-	// file_put_contents('test/1.jpg', $img_data);
+	$monster_img_path = get_monster_image($monster_name);
+	// $monster_img_path = 'test/678.jpg';
+
+	generate_post_image($user_img_path, $monster_img_path, 'img/bg_cropped.jpg', 'test_852.jpg');
 }
--- start.php
+++ start.php
@@ -7,16 +7,27 @@ $config = include('config.php');
 $fb = new Facebook\Facebook($config['facebook']);
 $helper = $fb->getRedirectLoginHelper();
 
-function get_cardmaker_upload_path($url){
+function save_remote_image($url){
+	// $headers = getallheaders();
 	$options = [
+		// 'http' => [
+		// 	'method' => 'GET',
+		// 	'header' => "User-Agent: " . $headers['User-Agent'] . " \r\n"
+		// ],
 		'ssl' => [
 			'verify_peer' => false,
 			'verify_peer_name' => false
 		]
 	];
-	$tmp_path = tempnam(sys_get_temp_dir(), 'image.jpg');
+	// echo $url . '<br>';
 	$img_data = file_get_contents($url, false, stream_context_create($options));
-	file_put_contents($tmp_path, $img_data);
+	$path = tempnam(sys_get_temp_dir(), 'tmp_img');
+	file_put_contents($path, $img_data);
+	return $path;
+}
+
+function get_cardmaker_upload_path($url){
+	$tmp_path = save_remote_image($url);
 
 	$ch = curl_init('http://www.yugiohcardmaker.net/ycmaker/uploadimage.php');
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -33,8 +44,8 @@ function get_cardmaker_upload_path($url){
 
 function get_cardmaker_params($user){
 	$picture_url = 'http://graph.facebook.com/' . $user['id'] . '/picture?width=400';
-	$path = get_cardmaker_upload_path($picture_url);
-	// $path = 'tempimages/175108937.jpg';
+	// $path = get_cardmaker_upload_path($picture_url);
+	$path = 'tempimages/175108937.jpg';
 
 	return [
 		'name' => $user['name'],
@@ -52,7 +63,8 @@ function generate_card($params){
 	$page = 'http://www.yugiohcardmaker.net/ycmaker/createcard.php';
 	$url = $page . '?' . http_build_query($params);
 
-	return file_get_contents($url);
+	$path = save_remote_image($url);
+	return $path;
 }
 
 function url_exists($url){
@@ -63,11 +75,15 @@ function url_exists($url){
 function get_monster_for($user){
 	global $config;
 	if(isset($config['predefined_user_cards'][$user['id']]))
-		return get_monster_data($config['predefined_user_cards'][$user['id']]);
+		return $config['predefined_user_cards'][$user['id']];
+}
+
+function normalize_monster_name($str){
+	return str_replace('#', '', $str);
 }
 
 function get_monster_data($name){
-	$name = str_replace('#', '', $name);
+	$name = normalize_monster_name($name);
 	$url = 'http://yugiohprices.com/api/card_data/' . $name;
 	$json = file_get_contents($url);
 	$response = json_decode($json, true);
@@ -79,4 +95,38 @@ function get_monster_data($name){
 		$response['data']['portuguese_name'] = $matches[1];
 
 	return $response['data'];
+}
+
+function get_monster_image($name){
+	$name = normalize_monster_name($name);
+	$url = 'http://yugiohprices.com/api/card_image/' . urlencode($name);
+	return save_remote_image($url);
+}
+
+function generate_post_image($user_img_path, $monster_img_path, $background_img_path, $path){
+	$img = imagecreatefromjpeg($background_img_path);
+	$user_img = imagecreatefromjpeg($user_img_path);
+	$monster_img = imagecreatefromjpeg($monster_img_path);
+
+	$padd = 35;
+	$fiw = imagesx($img);
+	$fih = imagesy($img);
+
+	$uiw = imagesx($user_img);
+	$uih = imagesy($user_img);
+	$uihd = $fih - 2 * $padd;
+	$uiwd = $uiw * $uihd / $uih;
+	imagecopyresampled($img, $user_img, $padd * 1.8, $padd, 0, 0, $uiwd, $uihd, $uiw, $uih);
+
+	$miw = imagesx($monster_img);
+	$mih = imagesy($monster_img);
+	$mihd = $fih - 2 * $padd;
+	$miwd = $miw * $mihd / $mih;
+	imagecopyresampled($img, $monster_img, $fiw - $padd * 1.8 - $miwd, $padd, 0, 0, $miwd, $mihd, $miw, $mih);
+
+	imagejpeg($img, $path, 100);
+
+	imagedestroy($user_img);
+	imagedestroy($monster_img);
+	imagedestroy($img);
 }
