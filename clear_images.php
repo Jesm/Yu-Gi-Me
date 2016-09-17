9ab5c61545d8169a1a318dbe485884bcd00fabb7
Jonathan Szablevski <jszablevski@hotmail.com>
2016-09-17 17:01:54 -0300

    Add script to remove old post images generated by the application

---
+++ clear_images.php
@@ -0,0 +1,18 @@
+<?php
+
+include 'start.php';
+
+file_put_contents('cron_test.txt', "Executing " . time() . "\n", FILE_APPEND);
+exit;
+
+$folder_path = 'img/posts';
+$files = scandir($folder_path);
+$curr_time = time();
+foreach($files as $filename){
+	preg_match('/[0-9]+_([0-9]+)\.jpg/i', $filename, $matches);
+	if(count($matches) && $curr_time - (int) $matches[1] > $config['min_post_image_keep_time']){
+		$img_path = $folder_path . '/' . $filename;
+		echo "Deleting " . $img_path . "...\n";
+		unlink($img_path);
+	}
+}
